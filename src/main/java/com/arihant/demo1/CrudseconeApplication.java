cb26f2feac140d7a79ff8e7f1a05044bd65ae4a0
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-12-27 12:20:29 +0530

    first commit

---
+++ src/main/java/com/arihant/demo1/CrudseconeApplication.java
@@ -0,0 +1,13 @@
+package com.arihant.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudseconeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudseconeApplication.class, args);
+    }
+
+}
